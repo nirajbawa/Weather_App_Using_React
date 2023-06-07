8ad6a0aafa16c99d1fe8809376f00104f2a927c7
Niraj bava <nirajbava222@gmail.com>
2023-06-07 14:44:02 +0530

    proudction ui bug fixed

--- frontend/src/components/home/Home_Left/Home_Left.jsx
+++ frontend/src/components/home/Home_Left/Home_Left.jsx
@@ -3,7 +3,7 @@ import Home_Left_Btn from './Home_Left_Btn'
 
 export default function Home_Left(props) {
     return (
-        <div className="flex justify-center flex-col items-center px-2 gap-8 md:px-5 py-4 md:py-0 w-full  lg:md:w-2/3 xl:w-5/12  h-full">
+        <div className="flex justify-center flex-col items-center px-2 gap-8 md:px-8 lg:px-12 py-4 md:py-0 w-full  lg:md:w-2/3 xl:w-5/12  h-full">
             <Home_Left_Stack props={props.props} />
             <Home_Left_Btn fetchData={props.fetchData}/>
         </div>
--- frontend/src/components/home/Home_Left/Home_Left_Stack.jsx
+++ frontend/src/components/home/Home_Left/Home_Left_Stack.jsx
@@ -6,7 +6,7 @@ export default function Home_Left_Stack(props) {
 
 
     return (
-        <ul className="flex flex-col w-full lg:w-96 h-[28rem] lg:h-[33rem] justify-end items-center text-center gap-2 rounded-md bg-[#f7f9fa] dark:border-2 dark:border-white dark:bg-[#1e293b] dark:text-white py-3 px-5 text-lg font-[Inter,sans-serif] capitalize font-semibold">
+        <ul className="flex flex-col w-full lg:w-80 h-[30rem] justify-end items-center text-center gap-2 rounded-md bg-[#f7f9fa] dark:border-2 dark:border-white dark:bg-[#1e293b] dark:text-white py-5 px-5 text-lg font-[Inter,sans-serif] capitalize font-semibold">
            
             {props.props.stack.map((value, index)=>{
                 return <Home_Left_Stack_Item name= {value} key={index} i={index} props={props.props} />
